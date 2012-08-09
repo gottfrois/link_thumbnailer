b1e04eca87d90f135205f825f0fb9d85eb4adb37
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2012-08-09 19:56:56 +0200

    adding method_missing behavior

--- lib/link_thumbnailer/object.rb
+++ lib/link_thumbnailer/object.rb
@@ -1,6 +1,17 @@
 module LinkThumbnailer
   class Object < Hashie::Mash
 
+    def method_missing(method_name, *args, &block)
+      method_name = method_name.to_s
+
+      if method_name.end_with?('?')
+        method_name.chop!
+        !self[method_name].nil?
+      else
+        self[method_name]
+      end
+    end
+
     def valid?
       @@mandatory_attributes.each {|a| return false unless self[a] }
       true
