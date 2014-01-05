6746b51fff9d68a9ae70fe11f14102325a56dadb
Jorge Granado <jorge@cooksnaps.com>
2014-01-05 18:04:14 +0100

    Fix for multiple og:image tags

--- lib/link_thumbnailer/opengraph.rb
+++ lib/link_thumbnailer/opengraph.rb
@@ -2,15 +2,13 @@ module LinkThumbnailer
     class Opengraph
 
       def self.parse(object, doc)
+        object[:images] = []
         doc.css('meta').each do |m|
           if m.attribute('property') && m.attribute('property').to_s.match(/^og:(.+)$/i)
-            object[$1.gsub('-', '_')] = m.attribute('content').to_s
+            content = m.attribute('content').to_s
+            object[:images] << { source_url: content } if $1 == 'image'
+            object[$1.gsub('-', '_')] = content
           end
-        end
-
-        object[:images] = []
-        if object[:image]
-          object[:images] << { source_url: object[:image] }
         end
 
         object
