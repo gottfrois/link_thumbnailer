18cc6c334c5f781a4d5a8a9ff0857a00f34bec50
alfie <mailme@alfiemax.com>
2016-05-27 23:49:20 +0530

    Fixes Issue #88 - undefined method `size' for nil:NilClass

---
+++ lib/link_thumbnailer/uri.rb
@@ -0,0 +1,18 @@
+module LinkThumbnailer
+  class URI
+
+    attr_reader :attribute
+
+    def initialize(uri)
+      @attribute = uri.to_s
+    end
+
+    def valid?
+      !!(attribute =~ ::URI::regexp)
+    end
+
+    def to_s
+      attribute
+    end
+  end
+end
