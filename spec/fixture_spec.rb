3d6764d3e6b2c0ebb2308f96e72e6f0d9d3dc87a
Olexandr Hoshylyk <gashuk95@gmail.com>
2019-02-21 16:55:02 +0000

    Add specs for favicon icon

--- spec/fixture_spec.rb
+++ spec/fixture_spec.rb
@@ -109,6 +109,18 @@ describe 'Fixture' do
 
     end
 
+    context 'when favicon with root path in the href' do
+      let(:html) { File.open(File.dirname(__FILE__) + '/fixtures/with_root_path_in_href.html').read }
+
+      it { expect(action.favicon).to eq(favicon) }
+    end
+
+    context 'when favicon with related path in the href' do
+      let(:html) { File.open(File.dirname(__FILE__) + '/fixtures/with_related_path_in_href.html').read }
+
+      it { expect(action.favicon).to eq(favicon) }
+    end
+
   end
 
 end
